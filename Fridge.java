c86c1a14ea51308cb93ad05ffe4da96a56678abd
Krasimir <krasioodimitrov@gmail.com>
2022-07-22 00:03:47 +0300

    Update Bottle

--- Fridge.java
+++ Fridge.java
@@ -1,12 +1,11 @@
 package PersonalProject.Fridge;
 
-import java.util.List;
+
 
 public class Fridge {
     int capacity;
     int ID;
-    List<Bottle> bottles;
-    List<Shelf> shelves;
+
 
     Fridge(int ID, int capacity) {
         this.ID = ID;
